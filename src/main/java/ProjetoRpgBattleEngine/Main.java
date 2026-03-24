77805000aec48827a2dbe5c8a5a98a083592c75b
Gabriel Fernandes Da Costa <gcosta@softexpert.com>
2026-03-24 09:50:31 -0300

    novas funcoes e mudança de lógica

--- src/main/java/ProjetoRpgBattleEngine/Main.java
+++ src/main/java/ProjetoRpgBattleEngine/Main.java
@@ -1,36 +1,93 @@
 import ProjetoRpgBattleEngine.*;
+
+import java.sql.SQLOutput;
 import java.util.*;
 
 void main() {
-    Personagem joao = new Personagem("Joao", 100, 100, 5, 20, 20);
-    Personagem goblin = new Personagem("Goblin", 50, 50, 5, 20, 20);
+    Personagem goblin = new Personagem("Goblin", 50, 50, 5, 0, 0);
+    goblin.aprenderAcao(new AtaqueFisico(5));
+
+    String nomeClasse;
+
+    Personagem personagem;
 
     PersonagemDAO banco = new PersonagemDAO();
-
-    banco.salvar(joao);
-
-    joao.aprenderAcao(new AtaqueFisico(10));
-    joao.aprenderAcao(new Cura(10));
-    goblin.aprenderAcao(new Cura(20));
-    goblin.aprenderAcao(new AtaqueFisico(5));
 
     Scanner scan = new Scanner(System.in);
     Random rand = new Random();
 
     int acoesGoblin = goblin.acoes.size();
 
-    while (joao.morto() && goblin.morto()) {
-        System.out.println("\n-------------------- Turno do Joao --------------------");
-        joao.listarAcoes();
+    System.out.println("-------------------- Escolha sua Classe --------------------");
+    System.out.println("1 - Guerreiro");
+    System.out.println("2 - Mago");
+
+    int classeEscolhida = scan.nextInt();
+    if(classeEscolhida == 1){
+        nomeClasse = "Guerreiro";
+    }else{
+        nomeClasse = "Mago";
+    }
+
+    personagem = banco.buscarPorNome(nomeClasse);
+
+    String continuar;
+
+    if(personagem != null) {
+        System.out.println("Já existe um progresso salvo, deseja carregar?  S/N");
+        scan.nextLine();
+        continuar = scan.nextLine();
+        if (continuar.equals("S")) {
+            if (nomeClasse.equals("Guerreiro")) {
+                personagem.aprenderAcao(new AtaqueFisico(5));
+            }
+            if (nomeClasse.equals("Mago")) {
+                personagem.aprenderAcao(new Cura(5));
+            }
+        } else {
+            switch (classeEscolhida) {
+                case 1 -> {
+                    personagem = new Personagem("Guerreiro", 200, 200, 20, 0, 0);
+                    personagem.aprenderAcao(new AtaqueFisico(5));
+                }
+                case 2 -> {
+                    personagem = new Personagem("Mago", 70, 70, 5, 50, 50);
+                    personagem.aprenderAcao(new Cura(5));
+                }
+                default -> personagem = new Personagem("Teste", 1, 1, 1, 1, 1);
+            }
+            banco.salvar(personagem);
+        }
+    }else if(personagem == null){
+        switch (classeEscolhida) {
+            case 1 -> {
+                personagem = new Personagem("Guerreiro", 200, 200, 20, 0, 0);
+                personagem.aprenderAcao(new AtaqueFisico(5));
+            }
+            case 2 -> {
+                personagem = new Personagem("Mago", 70, 70, 5, 50, 50);
+                personagem.aprenderAcao(new Cura(5));
+            }
+            default -> personagem = new Personagem("Teste", 1, 1, 1, 1, 1);
+        }
+        banco.salvar(personagem);
+    }
+
+
+
+
+    while (!personagem.morto() && !goblin.morto()) {
+        System.out.println("\n-------------------- Seu Turno --------------------");
+        personagem.listarAcoes();
         int escolha = scan.nextInt();
         System.out.println("");
-        while(escolha < 0 || escolha >= joao.acoes.size())
+        while(escolha < 0 || escolha >= personagem.acoes.size())
         {
             System.out.println("Número Inválido, digite um número que existe no índice: ");
-            joao.listarAcoes();
+            personagem.listarAcoes();
             escolha = scan.nextInt();
         }
-        joao.usarAcao(escolha, goblin);
+        personagem.usarAcao(escolha, goblin);
 
         if (goblin.morto()) {
             System.out.println("O Goblin foi derrotado!");
@@ -41,10 +98,11 @@ void main() {
 
         System.out.println("\n-------------------- Turno do Goblin --------------------");
         int goblinAcao = rand.nextInt(acoesGoblin);
-        goblin.usarAcao(goblinAcao, joao);
+        goblin.usarAcao(goblinAcao, personagem);
     }
 
     System.out.println("Fim da Batalha");
+    banco.salvarProgresso(personagem);
 }
 
 public static void pausar(Scanner scan) {
--- src/main/java/ProjetoRpgBattleEngine/PersonagemDAO.java
+++ src/main/java/ProjetoRpgBattleEngine/PersonagemDAO.java
@@ -23,4 +23,60 @@ public class PersonagemDAO{
             System.out.println("Erro: " + e.getMessage());
         }
     }
+
+    public Personagem buscarPorNome(String nomeBusca) {
+        String sql = "SELECT * FROM personagens WHERE nome = ?";
+
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, nomeBusca);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return new Personagem(
+                        rs.getString("nome"),
+                        rs.getInt("vida_atual"),
+                        rs.getInt("vida_maxima"),
+                        rs.getInt("poder"),
+                        rs.getInt("mana_atual"),
+                        rs.getInt("mana_maxima")
+                );
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao carregar: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public void salvarProgresso(Personagem p) {
+        String sql = "UPDATE personagens SET vida_atual = ?, mana_atual = ? WHERE nome = ?";
+
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, p.getVidaAtual());
+            pstmt.setInt(2, p.getManaAtual());
+            pstmt.setString(3, p.getNome());
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erro ao salvar progresso: " + e.getMessage());
+        }
+    }
+
+    public void deletarPersonagem(String nome) {
+        String sql = "DELETE FROM personagens WHERE nome = ?";
+
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, nome);
+            pstmt.executeUpdate();
+            System.out.println("Personagem " + nome + " apagado");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao deletar: " + e.getMessage());
+        }
+    }
 }
